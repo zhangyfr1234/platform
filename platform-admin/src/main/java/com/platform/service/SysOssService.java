f8f085ec0bcb537402872cb10c0c41df519f8fc3
zhangyunfeng <zhangyunfeng03@meituan.com>
2019-06-27 23:45:13 +0800

    init

---
+++ platform-admin/src/main/java/com/platform/service/SysOssService.java
@@ -0,0 +1,25 @@
+package com.platform.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.platform.entity.SysOssEntity;
+import com.platform.utils.PageUtilsPlus;
+
+import java.util.Map;
+
+/**
+ * 文件上传Service
+ *
+ * @author lipengjun
+ * @email 939961241@qq.com
+ * @date 2017-03-25 12:13:26
+ */
+public interface SysOssService extends IService<SysOssEntity> {
+
+    /**
+     * queryPage
+     *
+     * @param params
+     * @return
+     */
+    PageUtilsPlus queryPage(Map<String, Object> params);
+}
